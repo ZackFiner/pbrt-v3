dc7e3fd099f6eb3aedde4fba083968b72bfa42e3
ZackFiner <zackary.finer@yahoo.com>
2020-04-30 01:15:59 -0700

    Specialized Fractal Material
    To add support for approximated Ambient Occlusion using Raymarching and rendering using Orbit Trapping, the creation of a specialized material is necessary.

---
+++ src/materials/orbittrap.h
@@ -0,0 +1,57 @@
+#if defined(_MSC_VER)
+#define NOMINMAX
+#pragma once
+#endif
+
+#ifndef PBRT_MATERIALS_ORBITTRAP_H
+#define PBRT_MATERIALS_ORBITTRAP_H
+
+// materials/matte.h*
+#include "pbrt.h"
+#include "material.h"
+
+namespace pbrt {
+
+	// MatteMaterial Declarations
+	class OrbitTrapMaterial : public Material {
+	public:
+		OrbitTrapMaterial(const std::shared_ptr<Texture<Spectrum>> &Kd,
+			const std::shared_ptr<Texture<Spectrum>> &Ks,
+			const std::shared_ptr<Texture<Spectrum>> &Kr,
+			const std::shared_ptr<Texture<Spectrum>> &Kt,
+			const std::shared_ptr<Texture<Float>> &roughness,
+			const std::shared_ptr<Texture<Float>> &roughnessu,
+			const std::shared_ptr<Texture<Float>> &roughnessv,
+			const std::shared_ptr<Texture<Spectrum>> &opacity,
+			const std::shared_ptr<Texture<Float>> &eta,
+			const std::shared_ptr<Texture<Float>> &bumpMap,
+			bool remapRoughness)
+			: Kd(Kd),
+			Ks(Ks),
+			Kr(Kr),
+			Kt(Kt),
+			opacity(opacity),
+			roughness(roughness),
+			roughnessu(roughnessu),
+			roughnessv(roughnessv),
+			eta(eta),
+			bumpMap(bumpMap),
+			remapRoughness(remapRoughness) {}
+
+		void ComputeScatteringFunctions(SurfaceInteraction *si, MemoryArena &arena,
+			TransportMode mode,
+			bool allowMultipleLobes) const;
+
+	private:
+		// UberMaterial Private Data
+		std::shared_ptr<Texture<Spectrum>> Kd, Ks, Kr, Kt, opacity;
+		std::shared_ptr<Texture<Float>> roughness, roughnessu, roughnessv, eta,
+			bumpMap;
+		bool remapRoughness;
+	};
+
+	OrbitTrapMaterial *CreateOrbitMaterial(const TextureParams &mp);
+
+}  // namespace pbrt
+
+#endif  // PBRT_MATERIALS_ORBITTRAP_H
